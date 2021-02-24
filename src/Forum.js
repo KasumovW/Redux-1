7ea9d7a150e0baa2f3f084bf975044f8c480ef75
Kass <lord.kasumov@mail.ru>
2021-02-24 23:38:42 +0300

    no Error

--- src/Forum.js
+++ src/Forum.js
@@ -17,7 +17,7 @@ function Forum(props) {
                             <div className="row justify-content-around textName pt-2 text-center m-auto">
                                 <div className="col-1 mail">{item.id}</div>
                                 <div className="col-3 id">{item.email}</div>
-                                <div className="col-auto X" onClick={() => nandleDelete(data.id)}>X</div>
+                                <div className="col-auto X" onClick={() => nandleDelete(item.id)}>X</div>
                             </div>
                             <div className="row">
                                 <div className="col-6 text-center name m-auto">
